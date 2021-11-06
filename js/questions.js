716394b6bd2410dfa0f3c234340838f9801ca703
kacha22 <mideslan@hotmail.com>
2021-11-05 21:10:57 -0300

    primeiro commit

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "O que significa HTML?",
+    answer: "Linguagem de HiperTexto",
+    options: [
+      "Linguagem Colonial",
+      "Linguagem de HiperTexto",
+      "Linguagem de Escrita",
+      "Query SQL"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual a tradução de CSS?",
+    answer: "Folhas de estilo em cascata",
+    options: [
+      "Linguagem de fraca tipagem",
+      "Auto preenchimento de layout",
+      "Folhas de estilo em cascata",
+      "Avaliação dos contras"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual comando executa uma consulta no Banco de Dados?",
+    answer: "Select * From Tabela",
+    options: [
+      "Select * From Tabela",
+      "Begin Tran",
+      "Select * Into Tabela",
+      "Update Tabela"
+    ]
+  },
+    {
+    numb: 4,
+    question: "O que é um Branch?",
+    answer: "Uma ramificação do código",
+    options: [
+      "Uma fabricante de carro",
+      "Uma ramificação do código",
+      "Atualização no repositório",
+      "É uma API"
+    ]
+  },
+    {
+    numb: 5,
+    question: "O que é uma Procedure?",
+    answer: "Um bloco de procedimento armazenado",
+    options: [
+      "Ativa uma ação especifica no banco de dados",
+      "Insere uma nova tabela no banco de dados",
+      "Executa o rollback do banco de dados",
+      "Um bloco de procedimento armazenado"
+    ]
+  },
+  {
+   numb: 6,
+  question: "Quem criou o JavaScript?",
+  answer: "Brendan Eich",
+   options: [
+    "Brendan Eich",
+    "Bill Gates",
+      "Elon Musk",
+       "Tiririca"
+     ]
+  },
+  {
+    numb: 7,
+   question: 'O que significa "concatenar"?',
+   answer: "Unir dados de modo lógico ou manter ligação ou conexão entre eles",
+    options: [
+     "Um conjunto de caracteres",
+     "Configuração de um tipo de variável",
+       "Unir dados de modo lógico ou manter ligação ou conexão entre eles",
+        "Faz a divisão de dois valores"
+      ]
+   },
+   {
+    numb: 8,
+   question: 'O que é um algoritmo?',
+   answer: "Uma sequência de instruções para chegar em um determinado objetivo",
+    options: [
+     "É a solução de um problema",
+     "São ações ditadas para o computador executar",
+      "Cria a integração entre os fornecedores",
+        "Uma sequência de instruções para chegar em um determinado objetivo"
+      ]
+   },
+   {
+    numb: 9,
+   question: 'O que é uma Função?',
+   answer: "um conjunto de instruções que executa uma tarefa ou calcula um valor",
+    options: [
+     "É a solução de um problema",
+     "São ações ditadas para o computador executar",
+      "Cria a integração entre os fornecedores",
+        "um conjunto de instruções que executa uma tarefa ou calcula um valor"
+      ]
+   },
+   {
+    numb: 10,
+   question: 'JavaScript é ?',
+   answer: "Uma linguagem de programação",
+    options: [
+     "Um modelo de Tijolo",
+     "Um tipo de salgadinho",
+      "Uma linguagem de programação",
+        "Um Array"
+      ]
+   }
+
+];
